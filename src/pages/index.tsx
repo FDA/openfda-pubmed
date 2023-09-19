8ccd5509dac5d363fb05cdc2d18b7d9d69a7bb10
Denis Krylov <dkrylovsb@users.noreply.github.com>
2023-09-19 09:44:50 -0400

    Home page changes to match the PDF mockup.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import type {HeadFC, PageProps} from "gatsby"
 import '@trussworks/react-uswds/lib/uswds.css'
 import '@trussworks/react-uswds/lib/index.css'
 import '../css/index.scss'
@@ -8,32 +8,32 @@ import '../css/pages/HomePage.scss'
 import Layout from "../components/Layout";
 
 const IndexPage: React.FC<PageProps> = () => {
-  return (
-    <Layout>
-      <section className='main-content'>
-        <div className='paragraph-hero flex'>
-          <div className='banner-paragraph bg-primary clr-white padding-3'>
-            <h2>Animal Drug Labeling and Adverse Events!</h2>
-            <div className='flex flex-column flex-fill'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Orci a scelerisque purus semper eget duis. Sollicitudin nibh sit amet commodo.
-                Orci dapibus ultrices in iaculis nunc sed augue lacus. Aliquam sem et tortor consequat id porta.
-                A cras semper auctor neque vitae. Facilisis magna etiam tempor orci eu. Id diam maecenas ultricies mi.</p>
-              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
-          </div>
-        </div>
-      </section>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <section className='main-content'>
+                <div className='flex'>
+                    <div className='bg-white padding-3'>
+                        <h2>FDA Patient Medication Information Repository</h2>
+                        <div className='flex flex-column flex-fill'>
+                            <p>Patient Medication Information provide patients with clear, concise, accessible, and
+                                useful written information for prescriptio ndrugs and certain biological products and
+                                would be delivered in a consistent and easy-to-understand format to help patients use
+                                their prescription drug and certain biological products safely and effectively.</p>
+                        </div>
+                    </div>
+                </div>
+               
+            </section>
+        </Layout>
+    )
 }
 
 export default IndexPage
 
 export const Head: HeadFC = () => {
-  return(
-    <>
-      <title>Home Page</title>
-    </>
-  )
+    return (
+        <>
+            <title>Home Page</title>
+        </>
+    )
 }
